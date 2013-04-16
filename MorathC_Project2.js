a00b41f22b8adaf0152ec1b1d8be6df435523ca9
Cassandra <cmorath@fullsail.edu>
2013-04-16 19:30:42 -0400

    Initial Commit
    Initial commit to repository with project files.

---
+++ MorathC_Project2.js
@@ -0,0 +1,19 @@
+window.onload = writeMessage;
+alert("JavaScript works!");
+//Cassandra Morath
+//SDI 1304
+//04/15/2013
+//Project 2
+function writeMessage() {
+var foodType = ["Dog Food", "Cat Food", "Bird Food"];
+var dogNames = ("Angel", "Delilah");
+var catNames = ("Chester", "Puddy");
+var birdNames = ("LaRita", "Einstien");
+var feedingPerDay = (3, 2, 1);
+
+//outputs
+console.log("I need to feed the dogs three times a day.");
+console.log("I need to feed the cats two times a day.");
+console.log("Ineed to feed the birds one time a day.");
+
+}
