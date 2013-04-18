73136f4b7e965b8becf4a78fccf09533b9882135
Cassandra <cmorath@fullsail.edu>
2013-04-17 21:23:24 -0400

    Functionally Complete

--- MorathC_Project2.js
+++ MorathC_Project2.js
@@ -1,62 +1,140 @@
 window.onload = writeMessage;
-alert("JavaScript works!");
+//alert("JavaScript works!");
 //Cassandra Morath
 //SDI 1304
 //04/15/2013
 //Project 2
+
+//main
 function writeMessage() {
 var petSitter = "Aaron";
-var vacationDays = (1, 2, 3, 4, 5);
-var pets = ["Angel", "Delilah", "Puddy", "Chester", "LaRita", "Einstien"] 
+var vacationDays = 0; 
+var pets = ["Angel", "Delilah", "Puddy", "Chester", "LaRita", "Einstien"];
+var momOnVacation = true;
+var poundsPerFeeding = 0;
+var petsHappy = "";
 
 
-//outputs
-console.log("Aaron needs to feed the pets.");
+console.log( petSitter + " needs to feed the pets." );
+for( vacationDays = 1; vacationDays <= 6; vacationDays++ ){
+	momOnVacation = isMomOnVacation( vacationDays, 5 );
+	feedPets( vacationDays, pets );
+	poundsPerFeeding = poundsOfPetFood( pets.length );
+	petCleanUp( momOnVacation );
+	if( momOnVacation ){
+	   petsHappy = happyPet(petSitter, " made the pets fat and happy");
+	}
+	else{
+		petsHappy = happyPet(petSitter, " moved out");
+	}
+	console.log("momOnVacation = " + momOnVacation);
+	console.log("poundsPerFeeding = " + poundsPerFeeding);
+	console.log("petsHappy = " + petsHappy);
+}
+	
+};
+//Procedure
+function petCleanUp(sitterCleansUp){
+	if(sitterCleansUp){
+		console.log("The pet sitter cleans up after the pets.");
+	}
+	else{
+		console.log("Mom is going to find a better pet sitter!");
+	}
+}
+		
+
+//Boolean Function
+function isMomOnVacation(vacationDays, lengthOfVacation){
+	if ( vacationDays <= lengthOfVacation ){
+		console.log("This is day " + vacationDays + " of mom's vacaction.");
+		return true;
+	}
+	else{
+		console.log("Mom is back from vacation...");
+		return false;
+	}
+}
+//array function
+function feedPets(vacationDays, pets){
+	var numberOfPetsFed = 0;
+	if( vacationDays >= 1 && vacationDays <= 5 )
+	{
+		for( var counter =0 ; counter < pets.length; counter++ ){
+			numberOfPetsFed++;
+			console.log( pets[ counter ] + " is being fed." );
+		}
+	}
+	else{
+		console.log("Mom can feed the pets herself!");
+	}
+	return pets;
+}
+//number Function
+function poundsOfPetFood( numberOfPets ){
+	var pounds = 0;
+	var poundsPerPet = .25;
+	var counter = 0;
+	
+	while(numberOfPets > counter){
+		counter++;
+		pounds += poundsPerPet;
+	}
+	return pounds;	
+}
+//String function
+function happyPet ( petSitter, didWhat ){
+	var happyPet = true;
+	var fatPet = true;
+	
+	return petSitter + didWhat;
+}
 
 // My mom is going to be going away on a trip. While she is gone, Aaron var petSitter = "Aaron" needs to //feed her pets.  
 //She will begone 5 days. var vacationDays = 5 
 //He needed to make sure all the animals were fed each day. var pets = ["Angel", "Delilah", 
 //"Puddy", "Chester", "LaRita", "Einstien"]
-//Mom left for vacation so it is time to feed the animals. var feedThePets = function (pets)
+//Mom left for vacation so it is time to feed the animals. function feedThePets = vacationDays <= 6
 // He has var petsRemaining = pets.length to go
 // He has to make sure each are fed appropriately (petsRemaining > 0)
 // Each animal gets fed once a day with the same food (var petNumber = 0; petNumber < pets.length; petNumber
 // ++)Aaron fed the pets one after the other until the pets were all fat and happy...
+//
 // except he forgot to feed the wildlife getInTrash = function (racoon, hungry) {...}; Aaron spent a lot of 
 // extra time cleaning up messes return "Shoo! Varmit! " + badRacoon;
 // Aaron counted the days until (neverAgain.movingOut())
 
-var petName = "Fluffy";
+//var petName = "Fluffy";
 
 // Call the "specialFood" proceedure
 //--------------------------------------------------------------------------
-specialFood( petName );
-isFluffyHungry( 4, 6 );
-yabadaba( "x", "y" );
+//specialFood( petName );
+//isFluffyHungry( 4, 6 );
+//yabadaba( "x", "y" );
 
 
-function yabadaba( oneValue, anotherValue )
-{
-}
+//function yabadaba( oneValue, anotherValue )
+//{
+//}
 
 // The "specialFood" proceedure
 // This procedure will check the value of the inbound argument and if it is equal
 // to "Fluffy" will output "Fluffy gets prime rib, otherwise it will output
 // the name of the animal and "gets mush"
-function specialFood( name )
-{
-	if( name === "Fluffy" )
-	{
-	}
-	else
-	{
-	}
-};
+//function specialFood( name )
+//{
+//	if( name === "Fluffy" )
+//	{
+//	}
+//	else
+//	{
+//	}
+//};
 
 // The isFluffyHugry function accepts two agurments.  The first specifies how often
 // Fluffy eat (in other word ever X hours), and the second specfies how long it's been
 // since fluffy at in hours.  The function will return true if Fluffy is hungry.
-function isFluffyHungry( howOftenFluffyEats, howLongSinceEaten )
+/*function isFluffyHungry( howOftenFluffyEats, howLongSinceEaten )
 {
 	var returnValue = true;
 	
@@ -71,8 +149,8 @@ function isFluffyHungry( howOftenFluffyEats, howLongSinceEaten )
 		//- Also want to output that Fluffy isn't hungry
 	}
 	return returnValue;
-}
+*/
 
 //The dogs are fed according to how much they will eat. If they are still hungry, I will give them more. This will be my number function. 
 
-}
+
